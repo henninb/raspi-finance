0a071b935bef96b9a8382b27e5e19cc1f8526185
Brian.Henning <brian.henning@target.com>
2018-09-23 16:05:37 -0500

    Migrating code to Kotlin.

---
+++ src/main/kotlin/finance/repositories/TransactionRepository.kt
@@ -0,0 +1,37 @@
+package finance.repositories
+
+
+import finance.model.Transaction
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
+
+interface TransactionRepository : JpaRepository<Transaction, Long> {
+
+    //@NamedNativeQuery(name="showSummaryResults", query="SELECT  account_name_owner, SUM(amount) AS totals FROM t_transaction GROUP BY account_name_owner ORDER BY account_name_owner", resultSetMapping="showSummaryResults")
+    //@get:Query(value = "SELECT  account_name_owner as accountNameOwner, SUM(amount) AS totals FROM t_transaction GROUP BY account_name_owner ORDER BY account_name_owner", nativeQuery = true)
+    //val summary: List<Summary>
+
+    //function name must match name on database table
+    fun findByDescriptionIgnoreCase(description: String): Transaction
+
+    fun findByTransactionId(transactionId: Long?): Transaction
+
+    fun findByAccountNameOwnerIgnoreCaseOrderByTransactionDate(accountNameOwner: String): List<Transaction>
+
+    fun findByGuid(guid: String): Transaction
+
+    // Using SpEL expression
+    @Query("SELECT SUM(amount) FROM #{#entityName} WHERE isCleared = 1 AND accountNameOwner=?1")
+    fun fetchAccoutTotals(accountNameOwner: String): Double
+
+    // Using SpEL expression
+    @Query("SELECT SUM(amount) AS accountTotal FROM #{#entityName} WHERE isCleared=1 AND accountNameOwner=?1")
+    fun fetchAccoutClearedTotals(accountNameOwner: String): Double
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE from t_transaction WHERE guid = ?1", nativeQuery = true)
+    fun deleteByGuid(guid: String)
+}
