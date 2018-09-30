ca0661c3dc252163e24afed219d15197546cb2aa
Brian.Henning <brian.henning@target.com>
2018-09-30 18:08:53 -0500

    fixed a timestamp issue

--- src/main/kotlin/finance/utils/TransactionDeserializer.kt
+++ src/main/kotlin/finance/utils/TransactionDeserializer.kt
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import finance.models.Transaction
 
 import java.io.IOException
-import java.sql.Date
+import java.sql.Timestamp
 import java.text.DecimalFormat
 
 class TransactionDeserializer @JvmOverloads constructor(vc: Class<*>? = null) : StdDeserializer<Transaction>(vc) {
@@ -27,9 +27,13 @@ class TransactionDeserializer @JvmOverloads constructor(vc: Class<*>? = null) : 
         val amount = (DecimalFormat("#.##").format(node.get("amount").asDouble())).toDouble()
         val cleared = node.get("cleared").asInt()
         val reoccurring = node.get("reoccurring").asBoolean()
-        val transactionDate = Date(node.get("transactionDate").asLong() * 1000)
-        val dateUpdated = Date(node.get("dateUpdated").asLong() * 1000)
-        val dateAdded = Date(node.get("dateAdded").asLong() * 1000)
+        //System.out.println("transactionDate: " + node.get("transactionDate").asLong() * 1000)
+        val transactionDate = Timestamp(node.get("transactionDate").asLong() * 1000)
+
+        val dateUpdated = Timestamp(node.get("dateUpdated").asLong() * 1000)
+        //System.out.println("dateUpdated: " + dateUpdated + " - " + node.get("dateUpdated").asLong() * 1000)
+        val dateAdded = Timestamp(node.get("dateAdded").asLong() * 1000)
+        //System.out.println("dateAdded: " + dateAdded + " - " +  node.get("dateAdded").asLong() * 1000)
 
         return Transaction(guid, accountType, accountNameOwner, transactionDate, description, category, amount, cleared, reoccurring, notes, dateUpdated, dateAdded, sha256)
     }
