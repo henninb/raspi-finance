7100cedb1243ddaaa69e89bbc758df4e336fff15
Brian Henning <henninb@msn.com>
2018-10-10 20:06:37 -0500

    added activemq capability for secure and insecure connections via configuration settings.

---
+++ run_windows.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+set DATASOURCE=jdbc:postgresql://192.168.100.25:5432/finance_db
+echo %DATASOURCE%
+set DATASOURCE_USERNAME=henninb
+echo %DATASOURCE_USERNAME%
+set DATASOURCE_PASSWORD=monday1
+echo %DATASOURCE_PASSWORD%
+set DATASOURCE_DRIVER=org.postgresql.Driver
+echo %DATASOURCE_DRIVER%
+set JSON_FILES_INPUT_PATH=json_in
+echo %JSON_FILES_INPUT_PATH%
+set SERVER_PORT=8080
+echo %SERVER_PORT%
+set AMQ_BROKER_URL=ssl://hornsup:61617
+echo %AMQ_BROKER_URL%
+set AMQ_USER=none
+set AMQ_PWD=none
+set SSL_TRUSTSTORE=amq-client.ts
+set SSL_TRUSTSTORE_PASSOWRD=monday1
+set SSL_KEYSTORE=amq-client.ks
+set SSL_KEYSTORE_PASSOWRD=monday1
+set LOGS=logs
+set MONGO_DATABASE=finance_db
+set MONGO_HOSTNAME=192.168.100.25
+set MONGO_PORT=27017
+set MONGO_URI=mongodb://192.168.100.25/finance_db
+set ACTIVEMQ_SSL_BEANS_ENABLED=true
+set ACTIVEMQ_NONSSL_BEANS_ENABLED=false
+
+java -jar build\libs\raspi_finance.jar --spring.config.location=src\main\resources\application.properties
+
+pause
--- src/main/kotlin/finance/configs/ActivemqSslJmsConfig.kt
+++ src/main/kotlin/finance/configs/ActivemqSslJmsConfig.kt
@@ -5,14 +5,17 @@ import org.apache.camel.component.jms.JmsComponent
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.jms.ConnectionFactory
 
 @Configuration
 @EnableTransactionManagement
+//@Profile("secure")
 open class ActivemqSslJmsConfig {
     @Value("\${spring.activemq.broker-url}")
     private val amqBrokerUrl: String? = null
@@ -41,7 +44,8 @@ open class ActivemqSslJmsConfig {
     private val LOGGER = LoggerFactory.getLogger(this.javaClass)
 
     //activemq-ssl JMS endpoint for camel
-    @Bean(name = arrayOf("activemq-ssl"))
+    @ConditionalOnProperty(value="activemq-ssl.beans.enabled")
+    @Bean(name = arrayOf("activemq"))
     open fun activeMQSslJmsComponent(cachingConnectionFactory: ActiveMQSslConnectionFactory): JmsComponent {
         val jmsComponent = JmsComponent()
         jmsComponent.setConnectionFactory(cachingConnectionFactory)
@@ -49,48 +53,37 @@ open class ActivemqSslJmsConfig {
         jmsComponent.setReceiveTimeout(receiveTimeout)
         return jmsComponent
     }
-        //activemq-ssl
-        @Bean
-        @Primary
-        open fun activeMQSslConnectionFactoryNoParmsBean(): ActiveMQSslConnectionFactory {
-            val activeMQSslConnectionFactory = ActiveMQSslConnectionFactory()
+    //activemq-ssl
+    @ConditionalOnProperty(value="activemq-ssl.beans.enabled")
+    @Bean
+    @Primary
+    open fun activeMQSslConnectionFactoryNoParmsBean(): ActiveMQSslConnectionFactory {
+        val activeMQSslConnectionFactory = ActiveMQSslConnectionFactory()
 
-            try {
-                LOGGER.info("activeMQSslConnectionFactory: setup for ssl")
-                activeMQSslConnectionFactory.keyStore = sslKeystore
-                activeMQSslConnectionFactory.keyStorePassword = sslKeystorePassword
-                activeMQSslConnectionFactory.trustStore = sslTruststore
-                activeMQSslConnectionFactory.trustStorePassword = sslTruststorePassword
-                activeMQSslConnectionFactory.userName = amqUsername
-                activeMQSslConnectionFactory.password = amqPassword
-                activeMQSslConnectionFactory.brokerURL = amqBrokerUrl
-            } catch (e: Exception) {
-                LOGGER.error(e.message)
-                //e.printStackTrace()
-            }
+        activeMQSslConnectionFactory.keyStore = sslKeystore
+        activeMQSslConnectionFactory.keyStorePassword = sslKeystorePassword
+        activeMQSslConnectionFactory.trustStore = sslTruststore
+        activeMQSslConnectionFactory.trustStorePassword = sslTruststorePassword
+        activeMQSslConnectionFactory.userName = amqUsername
+        activeMQSslConnectionFactory.password = amqPassword
+        activeMQSslConnectionFactory.brokerURL = amqBrokerUrl
 
-            return activeMQSslConnectionFactory
-        }
+        return activeMQSslConnectionFactory
+    }
 
     //activemq-ssl
+    @ConditionalOnProperty(value="activemq-ssl.beans.enabled")
     @Bean
     open fun activeMQSslConnectionFactoryBean(connectionFactory: ActiveMQSslConnectionFactory): ConnectionFactory {
         val activeMQSslConnectionFactory = ActiveMQSslConnectionFactory()
-        try {
-            LOGGER.info("activeMQSslConnectionFactoryBean: setup for ssl")
-            LOGGER.info("sslKeystore: $sslKeystore")
-            activeMQSslConnectionFactory.keyStore = sslKeystore
-            activeMQSslConnectionFactory.keyStorePassword = sslKeystorePassword
-            activeMQSslConnectionFactory.trustStore = sslTruststore
-            LOGGER.info("sslTruststore: $sslTruststore")
-            activeMQSslConnectionFactory.trustStorePassword = sslTruststorePassword
-            activeMQSslConnectionFactory.brokerURL = amqBrokerUrl
-            activeMQSslConnectionFactory.userName = amqUsername
-            activeMQSslConnectionFactory.password = amqPassword
-        } catch (e: Exception) {
-            LOGGER.error(e.message)
-            //e.printStackTrace()
-        }
+
+        activeMQSslConnectionFactory.keyStore = sslKeystore
+        activeMQSslConnectionFactory.keyStorePassword = sslKeystorePassword
+        activeMQSslConnectionFactory.trustStore = sslTruststore
+        activeMQSslConnectionFactory.trustStorePassword = sslTruststorePassword
+        activeMQSslConnectionFactory.brokerURL = amqBrokerUrl
+        activeMQSslConnectionFactory.userName = amqUsername
+        activeMQSslConnectionFactory.password = amqPassword
 
         return activeMQSslConnectionFactory
     }
