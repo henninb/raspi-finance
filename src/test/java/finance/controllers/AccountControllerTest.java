2d04fc3ca1b2e39fb2c9a5537bb9df1843224631
Brian Henning <henninb@msn.com>
2018-10-12 21:56:30 -0500

    updates to testing.

---
+++ src/test/java/finance/controllers/AccountControllerTest.java
@@ -0,0 +1,13 @@
+package finance.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class AccountControllerTest {
+    @Test
+    public void dummy()  {
+        assert (true);
+    }
+}
