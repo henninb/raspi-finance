4cef44ab91582f19085f6080721300623fd399ac
Brian Henning <henninb@msn.com>
2018-10-14 08:32:09 -0500

    more testing added; file reading for rust.

--- src/test/java/finance/processors/JsonTransactionProcessorTest.java
+++ src/test/java/finance/processors/JsonTransactionProcessorTest.java
@@ -33,10 +33,11 @@ public class JsonTransactionProcessorTest {
 
     private Transaction transactionDummy;
     private Transaction[] transactionsDummy;
+    String json_string;
 
     @Before
     public void setup() throws JsonParseException, IOException {
-        String json_string = "{\"guid\":\"83e30475-fcfc-4f30-8395-0bb40e89b568\",\"sha256\":\"b154020e5cd52d086d1650fa01a82b82f823e67cd59b7b346aeeb781c7dafa44\",\"accountType\":\"credit\",\"accountNameOwner\":\"chase_brian\",\"description\":\"First Transaction\",\"category\":\"\",\"notes\":\"Something\",\"cleared\":1,\"reoccurring\":false,\"amount\":\"0.1\",\"transactionDate\":1353456000,\"dateUpdated\":1538351596,\"dateAdded\":1538351596}";
+        json_string = "{\"guid\":\"83e30475-fcfc-4f30-8395-0bb40e89b568\",\"sha256\":\"b154020e5cd52d086d1650fa01a82b82f823e67cd59b7b346aeeb781c7dafa44\",\"accountType\":\"credit\",\"accountNameOwner\":\"chase_brian\",\"description\":\"First Transaction\",\"category\":\"\",\"notes\":\"Something\",\"cleared\":1,\"reoccurring\":false,\"amount\":\"0.1\",\"transactionDate\":1353456000,\"dateUpdated\":1538351596,\"dateAdded\":1538351596}";
         transactionDummy = mapper.readValue(json_string, Transaction.class);
         String guid = "83e30475-fcfc-4f30-8395-0bb40e89b568";
 //        transactionDummy = new Transaction();
@@ -48,7 +49,7 @@ public class JsonTransactionProcessorTest {
 //        transactionDummy.setDescription("test example");
 //        transactionDummy.setNotes("");
         //this.resourceBundle = PropertyResourceBundle.getBundle("test_inputs");
-        //MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.initMocks(this);
         //Mockito.doNothing().when(transactionService).insertTransaction(transactionDummy);
         //Mockito.doNothing().when(transactionService).findByGuid(guid);
     }
