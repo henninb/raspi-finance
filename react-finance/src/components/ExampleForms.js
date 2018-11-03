e7cf03e5e7913991ba1f955c0c1806903b1bc48c
Brian.Henning <brian.henning@target.com>
2018-11-03 18:02:33 -0500

    added react front end.

---
+++ react-finance/src/components/ExampleForms.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './ExampleForms.css';
+
+class ExampleForms extends React.Component {  
+  render() {
+    return (
+
+<div>
+  <form action="/action_page.php">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+  
+    <input type="submit" value="Submit" />
+  </form>
+</div>
+
+    );
+  }
+}
+
+export default ExampleForms;
