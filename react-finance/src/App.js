b1f4f1253860d54a998d3501c5c39b585093a3e6
Brian.Henning <brian.henning@target.com>
2018-11-03 21:19:11 -0500

    react front end updates.

--- react-finance/src/App.js
+++ react-finance/src/App.js
@@ -2,65 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import ExampleForms from './components/ExampleForms';
 import DropdownMenu from './components/DropdownMenu';
-import MyDropDown from './components/MyDropDown';
-import DownshiftMultiple from './components/DownshiftMultiple';
+//import DownshiftMultiple from './components/DownshiftMultiple';
+import TransactionTable from './components/TransactionTable';
 
 const welcome = "Welcome to React-Finance";
 
 class App extends Component {
-
-    constructor(){
+  constructor() {
     super()
-    this.state = {
-      location: [],
-      fruit: []
-    }
-  }
-  
-  toggleSelected = (id, key) => {
-    let temp = JSON.parse(JSON.stringify(this.state[key]))
   }
 
-  resetThenSet = (id, key) => {
-    let temp = JSON.parse(JSON.stringify(this.state[key]))
-    temp.forEach(item => item.selected = false);
-    temp[id].selected = true;
-    this.setState({
-      [key]: temp
-    })
-  }
-  
   render() {
     return (
       
       <div className="App">
         <DropdownMenu />
           <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <ExampleForms />
-          </p>
           <p>
             {welcome}
           </p>
-          <DownshiftMultiple />
+          <TransactionTable />
         </header>
       </div>
-    );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    console.log('hi');
-    const {text} = this.props;
-    return (
-    <div>
-      <b>{this.props.text}</b>
-      <b>{text}</b>
-    </div>
     );
   }
 }
--- react-finance/src/components/DropdownMenu.js
+++ react-finance/src/components/DropdownMenu.js
@@ -1,7 +1,71 @@
 import React, { Component } from 'react';
+//import axios from "axios";
 import './DropdownMenu.css';
 
-class DropdownMenu extends React.Component {  
+
+
+class DropdownMenu extends Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: [],
+    };
+  }
+
+  /*
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+
+        // create an array of contacts only with relevant data
+        const newContacts = response.data.map(c => {
+          return {
+            id: c.id,
+            name: c.name
+          };
+        });
+
+        // create a new "State" object without mutating 
+        // the original State object. 
+        const newState = Object.assign({}, this.state, {
+          contacts: newContacts
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+      })
+      .catch(error => console.log(error));
+  }
+  */
+  
+  /*
+  componentDidMount() {
+    fetch('http://localhost:8080/select_accounts')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let accounts = data.results.map((accountNameOwner) => {
+      return (
+      <a href="#">Link blah</a>
+      )
+    })
+    this.setState({accounts, accounts});
+    console.log("state", this.state.accounts)
+    })
+  }
+  
+  
+  
+        .then(data => this.setState({ accounts }));
+  */
+  
+  componentDidMount() {
+    fetch('http://localhost:8080/select_accounts')
+      .then(response => response.json())
+      .then(data => this.setState({ accounts: data }));
+  }
+
   render() {
     return (
 
@@ -9,12 +73,14 @@ class DropdownMenu extends React.Component {
 <ul>
   <li><a href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
+  <li className="dropdown">
+    <a href="javascript:void(0)" className="dropbtn">Dropdown</a>
+    <div className="dropdown-content">
+      {
+
+        this.state.accounts.map(accounts => { 
+        return <a href='http://localhost:8080/get_by_account_name_owner/{accounts.accountNameOwner}'>{accounts.accountNameOwner}</a>})
+      }
     </div>
   </li>
   <li><a href="#next">Next</a></li>
