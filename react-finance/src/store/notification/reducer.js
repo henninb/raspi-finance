ed589fb75c54fb0cd59f4db842e338d443d583cd
Brian.Henning <brian.henning@target.com>
2018-11-14 20:51:26 -0600

    fixed the store for redux.

--- react-finance/src/store/notification/reducer.js
+++ react-finance/src/store/notification/reducer.js
@@ -1,12 +1,24 @@
-import { SHOW_NOTIFICATION } from './actionType'
+import { SHOW_NOTIFICATION, SHOW_ACCOUNTS } from './actionType'
 
-export const initialState = {
+export const initialNotificationState = {
   isShown: false,
   message: '',
 }
 
-export default function notificationReducer (state = initialState, action = {}) {
+//export default function notificationReducer (state = initialNotificationState, action = {}) {
+export default function notificationReducer (state = initialNotificationState, action = {}) {
+//export default function notificationReducer (state, action) {
+    //if (state === undefined || state === null) {
+    //  state = {}
+    //  const newState1 = {
+    //    ...state,
+    //    isShown : false,
+    //    message: '',
+    //  }
+	//  return newState1;
+    //}
   switch (action.type) {
+
     case SHOW_NOTIFICATION: {
       const { payload } = action
       const { isShown, message } = payload
@@ -16,6 +28,9 @@ export default function notificationReducer (state = initialState, action = {}) 
         message,
       }
       return newState
+    }
+    case SHOW_ACCOUNTS: {
+      alert('SHOW_ACCOUNTS');
     }
     default:
       return state
