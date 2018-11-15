62320bd43d5682294d88101d703c1dd43ecaad87
Brian Henning <henninb@msn.com>
2018-11-14 20:51:26 -0600

    fixed the store for redux.

--- react-finance/src/store/notification/actionCreator.js
+++ react-finance/src/store/notification/actionCreator.js
@@ -1,8 +1,23 @@
-import { SHOW_NOTIFICATION } from './actionType'
+import { SHOW_NOTIFICATION, SHOW_ACCOUNTS } from './actionType'
 
 export function showNotification (isShown, message) {
-  return {
+  //alert('message got here:' + message);
+  //alert('message got here:' + SHOW_NOTIFICATION);
+
+   let x = {
     type: SHOW_NOTIFICATION,
+    payload: {
+      isShown,
+      message,
+    },
+   }
+
+  return x
+}
+
+export function showAccounts (isShown, message) {
+  return {
+    type: SHOW_ACCOUNTS,
     payload: {
       isShown,
       message,
