514dbcbfdf8bf03ad3b8f058821392a6abfd8c6a
Brian.Henning <brian.henning@target.com>
2018-11-25 08:06:24 -0600

    react redux store updates

--- react-finance/src/store/account/reducer.js
+++ react-finance/src/store/account/reducer.js
@@ -1,10 +1,11 @@
-import { SET_ACCOUNT, SET_TRANSACTION, SET_TRANSACTION_LOAD_STATUS } from './actionType'
+import { SET_ACCOUNT, SET_TRANSACTION, SET_TRANSACTION_LOAD_STATUS, SET_UPDATED_TRANSACTION } from './actionType'
 
 export const initialState = {
   isShown: false,
   accountNameOwners: '',
   viewStatus: 'none',
   transactions: '',
+  updatedTransaction: '',
 }
 
 export default function accountReducer (state = initialState, action = {}) {
@@ -35,6 +36,15 @@ export default function accountReducer (state = initialState, action = {}) {
       const newState = {
         ...state,
         viewStatus,
+      }
+      return newState
+    }
+    case SET_UPDATED_TRANSACTION: {
+      const { payload } = action
+      const { updatedTransaction } = payload
+      const newState = {
+        ...state,
+        updatedTransaction,
       }
       return newState
     }
