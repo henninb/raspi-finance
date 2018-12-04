20bb3a4b3227d9bd2e6fc80e0127bd99e1f0c6d7
Brian Henning <henninb@msn.com>
2018-12-04 17:13:48 -0600

    updates to spring boot for activemq and account transactions

--- src/main/kotlin/finance/configs/H2Config.kt
+++ src/main/kotlin/finance/configs/H2Config.kt
@@ -1,25 +1,15 @@
 package finance.configs
 
-import finance.models.Account
-import finance.models.Transaction
-import finance.repositories.AccountRepository
-import finance.repositories.TransactionRepository
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.context.annotation.PropertySource
 import org.springframework.transaction.annotation.EnableTransactionManagement
-import org.apache.tomcat.jni.SSL.setPassword
 import org.h2.server.web.WebServlet
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 import javax.sql.DataSource
 import org.springframework.boot.web.servlet.ServletRegistrationBean
-import java.sql.Timestamp
 
 @Configuration
-//@EnableJpaRepositories(basePackages = "org.baeldung.repository")
-//@PropertySource("h2-offline.properties")
 @EnableTransactionManagement
 @Profile("offline")
 open class H2Config {
---
+++ src/main/kotlin/finance/pojos/AccountType.kt
@@ -0,0 +1,9 @@
+package finance.pojos
+
+enum class AccountType {
+    credit,
+    debit
+}
+
+//AccountType.valueOf()
+//println(MyEnum.valueOf("Foo") == MyEnum.Foo)
