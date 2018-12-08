c251d001ac5125741d430e46943db3c605ee4ae8
Brian Henning <henninb@msn.com>
2018-12-07 20:44:52 -0600

    updates to mongo and account services.

--- src/main/kotlin/finance/repositories/MongoTransactionRepository.kt
+++ src/main/kotlin/finance/repositories/MongoTransactionRepository.kt
@@ -1,10 +1,12 @@
 package finance.repositories
 
-//import finance.models.Transaction
-//import org.springframework.data.mongodb.repository.MongoRepository
+import finance.models.Transaction
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.data.mongodb.repository.MongoRepository
 
 
-//interface MongoTransactionRepository : MongoRepository<Transaction, String> {
+@ConditionalOnExpression("\${project.mongo.feature:false}")
+interface MongoTransactionRepository : MongoRepository<Transaction, String> {
 //    fun findByDescriptionIgnoreCase(description: String): Transaction
 //    fun findByTransactionId(transactionId: Long?): Transaction
-//}
+}
