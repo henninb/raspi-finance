c610edd439da336c78f406996d1f3a81fe85e530
Brian Henning <henninb@msn.com>
2018-12-08 13:27:32 -0600

    updates to spring boot to allow for mongodb capability

--- src/main/kotlin/finance/repositories/MongoTransactionRepository.kt
+++ src/main/kotlin/finance/repositories/MongoTransactionRepository.kt
@@ -2,11 +2,22 @@ package finance.repositories
 
 import finance.models.Transaction
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import java.util.*
 
-
-@ConditionalOnExpression("\${project.mongo.feature:false}")
-interface MongoTransactionRepository : MongoRepository<Transaction, String> {
-//    fun findByDescriptionIgnoreCase(description: String): Transaction
-//    fun findByTransactionId(transactionId: Long?): Transaction
+//@ConditionalOnExpression("\${project.mongo.feature:true}")
+interface MongoTransactionRepository<T : Transaction> : MongoRepository<T, Long> {
+    fun findByDescriptionIgnoreCase(description: String): Optional<Transaction>
+    override fun findAll(pageable: Pageable): Page<T>
+    fun findAllOrderByTransactionId(pageable : Pageable) : Page<Transaction>
+    fun findByTransactionId(transactionId: Long?): Transaction
+    fun findByAccountNameOwnerAndClearedOrderByTransactionDateDesc(accountNameOwner: String, cleared: Int) : List<Transaction>
+    fun findByAccountNameOwnerAndClearedOrderByTransactionDateAsc(accountNameOwner: String, cleared: Int) : List<Transaction>
+    fun findByAccountNameOwnerIgnoreCaseOrderByTransactionDateDesc(accountNameOwner: String): List<Transaction>
+    fun findByAccountNameOwnerIgnoreCaseOrderByTransactionDateAsc(accountNameOwner: String): List<Transaction>
+    fun findByAccountNameOwnerIgnoreCaseOrderByTransactionDate(pageable : Pageable, accountNameOwner: String) : Page<Transaction>
+    fun findByGuid(guid: String): Optional<Transaction>
+    fun deleteByGuid(guid: String)
 }
